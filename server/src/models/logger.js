21664e85e46cd50918697bf81e8716007a863d6f
Haris Mehuljic <haris.mehuljic@drake.edu>
2025-05-25 20:21:33 -0500

    tweak logger, again

--- server/src/models/logger.js
+++ server/src/models/logger.js
@@ -2,26 +2,32 @@ import pino from "pino";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Get the current file's directory
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+function getLogFilePath(filePrefix) {
+    // Get the current file's directory
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
 
-// Go up two levels to reach the project root from server/src/models/logger.js
-const projectRoot = path.resolve(__dirname, "../../../");
+    // Go up two levels to reach the project root from server/src/models/logger.js
+    const projectRoot = path.resolve(__dirname, "../../../");
 
-// Generate a timestamped log file name
-const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
-const logFileName = `server-${timestamp}.log`;
-const logFilePath = path.join(projectRoot, "logs", logFileName);
+    // Generate a timestamped log file name
+    const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+    const logFileName = `${filePrefix}-${timestamp}.log`;
+    const logFilePath = path.join(projectRoot, "logs", logFileName);
+
+    return logFilePath;
+}
 
 const transport = pino.transport({
 	targets: [
+        // Log to a file in the logs directory
 		{
 			target: "pino/file",
-			options: { destination: logFilePath },
+			options: { destination: getLogFilePath("server") },
 		},
+        // Also log to the console
 		{
-			target: "pino/file", // logs to the standard output by default
+			target: "pino/file", 
 		},
 	],
 });
